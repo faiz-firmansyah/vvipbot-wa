b23bad3959b7d1a10979755d67050ea03f874a02
faiz-firmansyah <78604984+faiz-firmansyah@users.noreply.github.com>
2021-02-05 21:47:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ const menu = require("./lib/menu.js")
 
 const apivhtear = 'Apikey vhtear';
 const apibarbar = 'Apikey mhankbarbar';
-const BotName = 'VVIPINDO BOT'; 
-const instagram = 'agus_alnsyh71- kaelMk_'; 
-const aktif = 'Kapan bot aktif';
-const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
+const BotName = 'DORAEMON BOT'; 
+const instagram = 'FirmansyahFaiz30'; 
+const aktif = 'tergantung kuota owner';
+const vcard = 'BEGIN:DATA DORAEMON\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
+            + 'FN:Faiz\n' // Nama kamu
+            + 'ORG:DORAEMON;\n' // Nama bot
+            + 'TEL;type=CELL;type=VOICE;waid=6285624441487:+62 856-2444-1487\n' //Nomor whatsapp kamu
             + 'END:VCARD'
 const
 {
